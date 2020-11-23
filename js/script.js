dc8f00f7ded1a33a357d236b3a9174c83185204c
Ivan <raptorbox@gmail.com>
2020-11-23 13:27:45 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
  
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+var deadline="December 07 2020 12:00:00 GMT+0300";
 initializeClock('countdown', deadline);
